d20bd379d8ac7c98325ba36b479abac5cca3c260
FiorellaA17 <adamifiorella17@gmail.com>
2024-06-18 17:03:16 -0300

    Se agregó imágenes y página de home

---
+++ js/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').addEventListener('click', function() {
+        window.location.href = 'cotizaciones.html';
+    });
+});
